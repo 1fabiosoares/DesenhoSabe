fb0ecc48d31d6ff2056d0ab1d2553f9aa9beb8fe
Fabio Soares <fabioars@live.com>
2016-06-19 13:10:49 -0300

    Criando o projeto

---
+++ app/src/main/java/br/com/developbox/desenhosabe/ListaDeCalculos.java
@@ -0,0 +1,13 @@
+package br.com.developbox.desenhosabe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaDeCalculos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_de_calculos);
+    }
+}
